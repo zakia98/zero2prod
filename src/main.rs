9086c5c08500e00cc36828f770e5cefad1c2340f
zakia98 <ameenahmed1998@gmail.com>
2026-07-10 22:27:45 +0100

    remove redundant &

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    format!("Hello {}!", name)
 }
 
 #[tokio::main]
